c2ae486f85d5a6ed248cfd5ff0d76fc413c611b3
Admin <ruslan.nikandrov2015@gmail.com>
2023-06-26 22:16:14 +0300

    Created "fruit" and "mega_fruit" structs at fruit.h

--- C_Snake/include/fruit.h
+++ C_Snake/include/fruit.h
@@ -1,1 +1,13 @@
 #pragma once
+
+typedef struct fruit {
+	int position_y;
+	int position_x;
+	unsigned char fruit_symbol;
+} fruit;
+
+typedef struct mega_fruit {
+	fruit;
+	unsigned destroy_time;
+} mega_fruit;
+
