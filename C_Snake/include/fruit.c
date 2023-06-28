74140215508b4bf7eab7a661c10d9888a0f91613
Admin <ruslan.nikandrov2015@gmail.com>
2023-06-28 22:39:45 +0300

    Added at struct fruit::score gift and mega_fruit::reload_time variables, init this at create structs fucntions

--- C_Snake/include/fruit.c
+++ C_Snake/include/fruit.c
@@ -1,23 +1,27 @@
 #include "fruit.h"
 #include <stdlib.h>
 
-fruit* create_fruit(const position_y, const position_x, const unsigned char fruit_symbol) {
+fruit* create_fruit(const position_y, const position_x, const unsigned char fruit_symbol, unsigned score_gift) {
 	fruit* new_fruit = (fruit*)malloc(sizeof(fruit));
 	
 	new_fruit->position_y = position_y;
 	new_fruit->position_x = position_x;
 	new_fruit->fruit_symbol = fruit_symbol;
+	new_fruit->score_gift = score_gift;
 
 	return new_fruit;
 }
 
-mega_fruit* create_mega_fruit(const position_y, const position_x, const unsigned char megas_fruit_symbol, unsigned destroy_time) {
+mega_fruit* create_mega_fruit(const position_y, const position_x, const unsigned char megas_fruit_symbol, unsigned score_gift, unsigned destroy_time, unsigned unsigned reload_time) {
 	mega_fruit* new_mega_fruit = (mega_fruit*)malloc(sizeof(mega_fruit));
 
 	new_mega_fruit->position_y = position_y;
 	new_mega_fruit->position_x = position_x;
 	new_mega_fruit->fruit_symbol = megas_fruit_symbol;
-	new_mega_fruit->destroy_time = destroy_time;
+	new_mega_fruit->score_gift = score_gift;
+
+	new_mega_fruit->self_destroy_time = destroy_time;
+	new_mega_fruit->reload_time = reload_time;
 
 	return new_mega_fruit;
 }
--- C_Snake/include/fruit.h
+++ C_Snake/include/fruit.h
@@ -4,13 +4,15 @@ typedef struct fruit {
 	int position_y;
 	int position_x;
 	unsigned char fruit_symbol;
+	unsigned score_gift;
 } fruit;
 
 typedef struct mega_fruit {
 	fruit;
-	unsigned destroy_time;
+	unsigned self_destroy_time;
+	unsigned reload_time;
 } mega_fruit;
 
-fruit* create_fruit(const position_y, const position_x, const unsigned char fruit_symbol);
+fruit* create_fruit(const position_y, const position_x, const unsigned char fruit_symbol, unsigned score_gift);
 
-mega_fruit* create_mega_fruit(const position_y, const position_x, const unsigned char megas_fruit_symbol, unsigned destroy_time);
+mega_fruit* create_mega_fruit(const position_y, const position_x, const unsigned char megas_fruit_symbol, unsigned score_gift, unsigned destroy_time, unsigned unsigned reload_time);
